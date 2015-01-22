ca172e258ec9e88f02577aca1941f0e63b7ff6f2
Gedoc Sintset Kenfac <gedoc2005@yahoo.com>
2015-01-22 18:41:36 +0100

    RitaMedAsterisker

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RitaMedAsterisker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            for (int kolumn = 0; kolumn < 25; kolumn++)
+            {
+                int farg = kolumn % 3;
+                switch (farg)
+                {
+
+                    case 0:
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        break;
+                    case 1:
+                        Console.ForegroundColor = ConsoleColor.Magenta;
+                        break;
+                    case 2:
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        break;
+
+                    default:
+                        break;
+                }
+
+                if (kolumn % 2 == 1)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int rad = 0; rad < 39; rad++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+                Console.ResetColor();
+
+            }
+
+        }
+    }
+}
